bc5253e7c310ea80b1870a0d4d43051392034d0f
lakestorey <lakebeforestorey@gmail.com>
2017-04-28 13:59:38 -0400

    Final commit (I think)

--- SuperSnakeGame/Screens/GameScreen.cs
+++ SuperSnakeGame/Screens/GameScreen.cs
@@ -76,16 +76,6 @@ namespace BrickBreaker.Screens
             ball = new Ball(ballX, ballY, xSpeed, ySpeed, ballSize);
 
             //also added by Lake
-            //// Creates blocks for generic level
-            //blocks.Clear();
-            //int x = 10;
-
-            //while (blocks.Count < 12)
-            //{
-            //    x += 57;
-            //    Block b1 = new Block(x, 10, 1, Color.White);
-            //    blocks.Add(b1);
-            //}
             loadLevel("level1.xml");
 
             // start the game engine loop
@@ -225,13 +215,7 @@ namespace BrickBreaker.Screens
                         paddle.x = ((this.Width / 2) - (80 / 2));
                         paddle.y = (this.Height - 20) - 60;
                         #endregion
-
-                        //gameTimer.Enabled = false;
-
-                        //OnEnd();
                     }
-
-                    break;
                 }
             }
 
@@ -244,12 +228,12 @@ namespace BrickBreaker.Screens
                 ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
                 ball.y = (this.Height - paddle.height) - 85;
 
-                //if (lives == 0)
-                //{
-                //    gameTimer.Enabled = false;
+                if (lives == 0)
+                {
+                    gameTimer.Enabled = false;
 
-                //    OnEnd();
-                //}
+                    OnEnd();
+                }
             }
 
             //redraw the screen
